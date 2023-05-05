4c38b68fc74bd8f7529e3b45a6d9db8df09e59c3
wondu shiferaw <33760699+wondus36@users.noreply.github.com>
2023-05-05 12:02:11 +0300

    Update 1-repetition_token_0.rb

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,2 +1,19 @@
-#!/usr/bin/env ruby
-puts ARGV[0].scan(/hbt{2,5}n/).join
+require 'onigmo'
+
+# Check if the input string matches a regular expression
+def check_string(input)
+  pattern = /^hb(t*)$/
+  regex = Onigmo::RegularExpression.new(pattern)
+  if regex.match(input)
+    puts "The input string matches the pattern!"
+  else
+    puts "The input string does not match the pattern."
+  end
+end
+
+# Get the input string from the user
+puts "Enter a string to check:"
+input = gets.chomp
+
+# Call the check_string method with the input string
+check_string(input)
